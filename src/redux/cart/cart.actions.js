2591dcfa4c07c10447aae136a70b457dd4d9054c
Alejandro <alejandrobailo94@gmail.com>
2020-04-29 14:36:45 +0200

    remove item, add item added tocheout Item component

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -1,12 +1,22 @@
-import CartActionTypes from "./cart.types";
+import CartActionTypes from './cart.types';
 
 export const toogleCartHidden = () => ({
-  type: CartActionTypes.TOOGLE_CART_HIDDEN,
+	type: CartActionTypes.TOOGLE_CART_HIDDEN
 });
 
 export const addItem = (item) => ({
-  type: CartActionTypes.ADD_ITEM,
-  payload: item,
+	type: CartActionTypes.ADD_ITEM,
+	payload: item
+});
+
+export const clearItemFromCart = (item) => ({
+	type: CartActionTypes.CLEAR_ITEM_FROM_CART,
+	payload: item
+});
+
+export const removeItem = (item) => ({
+	type: CartActionTypes.REMOVE_ITEM,
+	payload: item
 });
 
 // Payload puede ser lo que nosotros queramos, en este caso sera el item que vamos a introducir en el cart
--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -1,17 +1,27 @@
 export const addItemToCart = (cartItems, cartItemToAdd) => {
-  const existingCartItem = cartItems.find(
-    (cartItem) => cartItem.id === cartItemToAdd.id
-  );
+	const existingCartItem = cartItems.find((cartItem) => cartItem.id === cartItemToAdd.id);
 
-  // Para que el componente se re-renderice tenemos que crear un nuevo estado por eso usamos map, para crear un array nuevo y que se actualice al detectarlo.
-  if (existingCartItem) {
-    return cartItems.map((cartItem) =>
-      cartItem.id === cartItemToAdd.id
-        ? { ...cartItem, quantity: cartItem.quantity + 1 }
-        : cartItem
-    );
-  }
+	// Para que el componente se re-renderice tenemos que crear un nuevo estado por eso usamos map, para crear un array nuevo y que se actualice al detectarlo.
+	if (existingCartItem) {
+		return cartItems.map(
+			(cartItem) =>
+				cartItem.id === cartItemToAdd.id ? { ...cartItem, quantity: cartItem.quantity + 1 } : cartItem
+		);
+	}
 
-  //   Si el objeto en el carro es nuevo devolvemos una copia del array con el nuevo objeto al final
-  return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
+	//   Si el objeto en el carro es nuevo devolvemos una copia del array con el nuevo objeto al final
+	return [ ...cartItems, { ...cartItemToAdd, quantity: 1 } ];
+};
+
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+	const existingCartItem = cartItems.find((cartItem) => cartItem.id === cartItemToRemove.id);
+
+	if (existingCartItem.quantity === 1) {
+		return cartItems.filter((cartItem) => cartItem.id !== cartItemToRemove.id);
+	}
+
+	return cartItems.map(
+		(cartItem) =>
+			cartItem.id === cartItemToRemove.id ? { ...cartItem, quantity: cartItem.quantity - 1 } : cartItem
+	);
 };
