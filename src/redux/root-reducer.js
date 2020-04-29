57c607fe670a31dbc9559b8444641d20003b0ebc
Alejandro <alejandrobailo94@gmail.com>
2020-04-29 15:05:47 +0200

    added persist-redux to implemet a localStorage on out app

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,9 +1,27 @@
-import { combineReducers } from "redux";
+import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
-import userReducer from "./user/user.reducer";
-import cartReducer from "./cart/cart.reducer";
+import userReducer from './user/user.reducer';
+import cartReducer from './cart/cart.reducer';
+
+const persisConfig = {
+	key: 'root',
+	storage,
+	// Pasamos en un array que reducers queremos meter en persist
+	whitelist: [ 'cart' ]
+};
+
+/* Versión sin storage:
 
 export default combineReducers({
-  user: userReducer,
-  cart: cartReducer,
+	user: userReducer,
+	cart: cartReducer
+}); */
+
+const rootReducer = combineReducers({
+	user: userReducer,
+	cart: cartReducer
 });
+
+export default persistReducer(persisConfig, rootReducer);
