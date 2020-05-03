41c61a5737c009a1e70a4b250784a80f4677917d
Alejandro <alejandrobailo94@gmail.com>
2020-05-03 14:14:32 +0200

    env keys

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,41 +3,41 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-    apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: "react-ecommerce-7189c.firebaseapp.com",
-    databaseURL: "https://react-ecommerce-7189c.firebaseio.com",
-    projectId: "react-ecommerce-7189c",
-    storageBucket: "react-ecommerce-7189c.appspot.com",
-    messagingSenderId: "627167311682",
-    appId: "1:627167311682:web:0d465a3e91f19b2931badb",
-    measurementId: "G-V4VY79WMV6"
-}
+	apiKey: 'AIzaSyDmCYrah0EU6_1jadoKDnBPFG_N4tFN47',
+	authDomain: 'react-ecommerce-7189c.firebaseapp.com',
+	databaseURL: 'https://react-ecommerce-7189c.firebaseio.com',
+	projectId: 'react-ecommerce-7189c',
+	storageBucket: 'react-ecommerce-7189c.appspot.com',
+	messagingSenderId: '627167311682',
+	appId: '1:627167311682:web:0d465a3e91f19b2931badb',
+	measurementId: 'G-V4VY79WMV6'
+};
 
 export const createUserProfileDocument = async (userAuth, additionalData) => {
-    if (!userAuth) return;
+	if (!userAuth) return;
 
-    const userRef = firestore.doc(`user/${userAuth.uid}`);
+	const userRef = firestore.doc(`user/${userAuth.uid}`);
 
-    const snapShot = await userRef.get();
+	const snapShot = await userRef.get();
 
-    if (!snapShot.exists) {
-        const { displayName, email } = userAuth;
-        const createdAt = new Date();
+	if (!snapShot.exists) {
+		const { displayName, email } = userAuth;
+		const createdAt = new Date();
 
-        try {
-            await userRef.set({
-                displayName,
-                email,
-                createdAt,
-                ...additionalData
-            })
-        } catch (error) {
-            console.log('error creating user', error.message)
-        }
-    }
+		try {
+			await userRef.set({
+				displayName,
+				email,
+				createdAt,
+				...additionalData
+			});
+		} catch (error) {
+			console.log('error creating user', error.message);
+		}
+	}
 
-    return userRef;
-}
+	return userRef;
+};
 
 firebase.initializeApp(config);
 
